0538d11f46c9f374b719e2df1d1e6f9634dc795c
Paulo Martins <paulovelho@paulovelho.com>
2025-07-06 22:26:34 +0200

    removing api key

--- src/inc/foot.php
+++ src/inc/foot.php
@@ -8,9 +8,4 @@
 <script src="vendor/js/parallaxie.min.js"></script>
 <script src="vendor/js/wow.min.js"></script>
 <script src="vendor/js/jquery.cubeportfolio.min.js"></script>
-<!--Contact Js-->
-<!-- <script src="vendor/js/contact_us.js"></script> -->
-<!-- Custom Script-->
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB4fusEY9kSwNHgtK8KOgyoTsyP5Tb2NXo"></script> -->
-<!-- <script src="portfolio-column/js/map.js"></script> -->
 <script src="js/scripts.js"></script>
